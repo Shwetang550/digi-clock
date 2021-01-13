42cf073f49e5182e89785d67ed8ba5190a17fa70
Shwetang <shwetangsingh5@gmail.com>
2021-01-13 18:56:20 +0530

    update time on button click

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+
+  let time = new Date().toLocaleTimeString();
+
+  const [ctime, setCtime] = useState(time);
+
+  const updateTime = () => {
+    time = new Date().toLocaleTimeString();
+    setCtime(time);
+  }
+
+  return ( 
+    <div style={{ textAlign: "center" }}>
+      <h1>
+        {time}
+      </h1>
+      <button onClick={updateTime}>Get Time</button>
     </div>
-  );
+   );
 }
-
+ 
 export default App;
